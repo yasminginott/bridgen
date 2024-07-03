a636a626fb9c754de667283093dd63dfdd4684a3
shir.afek <shir.afek@mail.huji.ac.il>
2024-07-03 12:51:37 +0300

    added YoungProfile &  edit function

---
+++ Pages/ElderProfile/ElderProfile.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var editIcon = document.getElementById("edit-button");
+    var editableElements = ["elder_name", "elder_location", "elder_description"];
+    var isEditing = false;
+  
+    // Create save button
+    var saveButton = document.createElement("button");
+    saveButton.textContent = "שמור";
+    saveButton.style.display = "none";
+    saveButton.id = "save-button";
+    editIcon.parentNode.insertBefore(saveButton, editIcon.nextSibling);
+  
+    editIcon.addEventListener("click", function() {
+      isEditing = !isEditing;
+      
+      
+      editableElements.forEach(function(id) {
+        var element = document.getElementById(id);
+        element.contentEditable = isEditing;
+        
+        if (isEditing) {
+          element.classList.add("editable");
+        } else {
+          element.classList.remove("editable");
+        }
+      });
+  
+      // Toggle icon and button visibility
+      editIcon.classList.toggle("editing");
+      saveButton.style.display = isEditing ? "inline" : "none";
+    });
+  
+    saveButton.addEventListener("click", function() {
+      isEditing = false;
+      
+      editableElements.forEach(function(id) {
+        var element = document.getElementById(id);
+        element.contentEditable = false;
+        element.classList.remove("editable");
+      });
+  
+      // Toggle icon and button visibility
+      editIcon.classList.remove("editing");
+      saveButton.style.display = "none";
+    });
+  });
