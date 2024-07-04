ae9a1314e4f2d6caecc2399721957966316a5b6f
shir.afek <shir.afek@mail.huji.ac.il>
2024-07-04 18:09:39 +0300

    adding function to end_button

--- Pages/AddSkill/AddSkill.js
+++ Pages/AddSkill/AddSkill.js
@@ -51,3 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.getElementById('end_form_button').addEventListener('click', function() {
+    window.location.href = '/Pages/ElderProfile/ElderProfile.html'; // Replace with your target HTML file name
+});
