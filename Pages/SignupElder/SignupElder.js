d23a8694394f4971600b149bb0de547689ff94b7
shir.afek <shir.afek@mail.huji.ac.il>
2024-07-07 13:46:09 +0300

    linked add skill pages

--- Pages/SignupElder/SignupElder.js
+++ Pages/SignupElder/SignupElder.js
@@ -2,3 +2,7 @@
 // can u see me?
 // yup!!!
 // we miss you Jasmine
+
+document.getElementById('continue_register').addEventListener('click', function() {
+    window.location.href = '/Pages/AddSkill/AddSkill.html'; });
+    
