5fdb50e32ecc16bf6f07ad6c1084bc3978a5fcbc
rotemmin <rotemminai@gmail.com>
2024-07-10 19:01:16 +0300

    rotem

--- Pages/EnteryScreen/SignupElder.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('continue_register').addEventListener('click', function(event) {
-    event.preventDefault(); // מניעת ברירת המחדל של שליחת הטופס
-    window.location.href = '/Pages/AddSkill/AddSkill.html'; // ניתוב לעמוד אחר
-});
